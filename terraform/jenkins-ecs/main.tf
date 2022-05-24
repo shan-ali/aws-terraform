05dd3032cd5840e7edf03798ee24f789b35821eb
Shan Ali <shan.ali.ru@gmail.com>
2022-05-24 16:14:09 -0400

    update role policy

--- terraform/jenkins-ecs/main.tf
+++ terraform/jenkins-ecs/main.tf
@@ -110,7 +110,8 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonECSServiceRolePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
 }
 
 resource "aws_security_group" "jenkins_ecs_sg" {
